0afc983d840284ef6dad443110073d7e36fefe39
mann <pmannp0423@gmail.com>
2025-12-29 18:14:18 +0530

    playing with router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 const express=require("express");
 const app=express();
+app.get("/abc*d",(req,res)=>{
+    res.send("hi i am beast")
+});
+
+app.get("/user",(req,res)=>{
+    console.log(req.query); //in this we need to use /user?userid=1234
+    res.send({name:"John", age:30});
+});
+app.get("/user/:userid/:userpass/:username",(req,res)=>{
+
+    console.log(req.params);
+    res.send({name:"John", age:30});
+});
+
+
+app.post("/user",(req,res)=>{
+    res.send("Data received via POST request");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("User deleted");
+});
 
 app.use("/abc",(req,res)=>{
     res.send("Hello Server abc page");
